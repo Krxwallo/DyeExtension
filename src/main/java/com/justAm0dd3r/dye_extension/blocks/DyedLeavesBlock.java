593c89453050d763f98e58ad5a2012ac4597af4c
Krxwallo <a@b.c>
2020-07-03 20:40:13 +0200

    (Almost) Fixed dyed leaves loot tables.

--- src/main/java/com/justAm0dd3r/dye_extension/blocks/DyedLeavesBlock.java
+++ src/main/java/com/justAm0dd3r/dye_extension/blocks/DyedLeavesBlock.java
@@ -1,11 +1,11 @@
 package com.justAm0dd3r.dye_extension.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.LeavesBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
-public class DyedLeavesBlock extends Block {
-
+public class DyedLeavesBlock extends LeavesBlock {
     public DyedLeavesBlock() {
         super(Block.Properties.create(Material.LEAVES)
                 .hardnessAndResistance(0.2F)
