65ecbf2a7c2862f145b4480c491e1cb89c480f45
Krxwallo <a@b.c>
2020-07-09 12:45:59 +0200

    - Fixed Hoe Attack Damage

--- src/main/java/com/justAm0dd3r/dye_extension/items/DyedHoe.java
+++ src/main/java/com/justAm0dd3r/dye_extension/items/DyedHoe.java
@@ -2,10 +2,19 @@ package com.justAm0dd3r.dye_extension.items;
 
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemTier;
 import net.minecraft.item.Items;
 
 public class DyedHoe extends HoeItem {
     public DyedHoe(IItemTier tier, float attackSpeedIn, Properties builder) {
-        super(tier, 0, attackSpeedIn, builder);
+        super(tier, getAttackDamage(tier), attackSpeedIn, builder);
+    }
+
+    private static int getAttackDamage(IItemTier tier) {
+        if (tier.equals(ItemTier.STONE)) return -1;
+        else if (tier.equals(ItemTier.IRON)) return -2;
+        else if (tier.equals(ItemTier.DIAMOND) || tier.equals(ItemTier.NETHERITE)) return -3;
+
+        return 0;
     }
 }
